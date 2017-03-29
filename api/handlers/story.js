7302f2e1d6e5358016c8af33f4b90c0150ddfbf7
Nuno Nogueira <nunofmn@gmail.com>
2017-03-29 23:16:57 +0100

    Add limit parameter to topstories endpoint.
    Add query string parameter 'limit' to topstories endpoint so is possible
    to define the number of topstories the client wants to receive.
    Defaults to 30 stories.
    
    Example:
     - /api/topstories?limit=30

--- api/handlers/story.js
+++ api/handlers/story.js
@@ -1,11 +1,15 @@
 const Boom = require('boom');
 const stories = require('../../util/stories');
 
+const DEFAULT_TOP_STORIES_NUMBER = 30;
+
 const story = {
   topStories: {
     handler: function(request, reply) {
 
-      stories.getTopStoriesItems(10, (err, stories) => {
+      const storiesSize = request.params.limit || DEFAULT_TOP_STORIES_NUMBER;
+
+      stories.getTopStoriesItems(storiesSize, (err, stories) => {
         if(err) {
           return reply(Boom.badImplementation('Error fetching top stories'));
         }
